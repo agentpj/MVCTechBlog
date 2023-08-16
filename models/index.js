553c4aaaa4934a38c651b8e8c2b273ddb0534753
Pearl Jones <jonesatlakota@yahoo.com>
2023-08-15 21:08:21 -0500

    was able to seed the database

--- models/index.js
+++ models/index.js
@@ -2,13 +2,18 @@ const User = require('./User');
 const Blog = require('./Blog');
 const Comment = require('./Comment');
 
+//user is associated to blog post via blog's user_id.  So if the user is deleted, the associated
+// blog should be deleted
 User.hasMany(Blog, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
+// blog is associated to comment via a comment's blog_id.  Hence, if the blog is deleted,
+// the associated comment should be deleted
 Blog.hasMany(Comment, {
-  foreignKey: 'blog_id'
+  foreignKey: 'blog_id',
+  onDelete: 'CASCADE'
 });
 
 Blog.belongsTo(User, {
